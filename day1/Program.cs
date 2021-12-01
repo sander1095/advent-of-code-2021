13663336380d09910d4bbcfec3902d433be98eae
Sander ten Brinke <s.tenbrinke2@gmail.com>
2021-12-01 22:57:05 +0100

    Complete day 1 of advent of code 2021!

---
+++ day1/Program.cs
@@ -0,0 +1,24 @@
+﻿var input = File.ReadAllLines("input.txt").Select(x => int.Parse(x)).ToList();
+
+var result = SonarSweep(input);
+
+Console.WriteLine(result);
+
+int SonarSweep(IReadOnlyList<int> numbers)
+{
+    var increases = 0;
+
+    // Skip the last item because it has already been compared
+    for (int i = 0; i < numbers.SkipLast(1).Count(); i++)
+    {
+        var firstItem = numbers[i];
+        var secondItem = numbers[i + 1];
+       
+        if (secondItem > firstItem)
+        {
+            increases++;
+        }
+    }
+
+    return increases;
+}
